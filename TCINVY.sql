297b50ba45195fde6111dd3847f887eb02465533
John M <johnswork15@gmail.com>
2017-11-25 16:34:26 -0600

    NOT NULL
    Changed the QuantityInStore and QuantityOnOrder to NOT NULL

--- TCINVY.sql
+++ TCINVY.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `worldwide_market`.`inventory` (
   `StoreID` INT NOT NULL,
   `ItemID` INT NOT NULL,
-  `QuantityInStore` INT NULL,
-  `QuantityOnOrder` INT NULL,
+  `QuantityInStore` INT NOT NULL,
+  `QuantityOnOrder` INT NOT NULL,
   PRIMARY KEY (`StoreID`, `ItemID`))
 COMMENT = 'The current stock of a store';
