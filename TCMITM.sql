c8bfa880902236e6f27394633e0d5ecca703d7c2
Ian Brothers <33327410+Aarith@users.noreply.github.com>
2017-11-25 16:38:11 -0600

    Creation of Magic Item table

---
+++ TCMITM.sql
@@ -0,0 +1,8 @@
+-- Table Creation - Magic Item Table
+
+CREATE TABLE `worldwide_market`.`magic_items` (
+  `ItemID` INT NOT NULL,
+  `SpellID` INT NOT NULL,
+  `ArtifactName` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`ItemID`, `SpellID`))
+COMMENT = 'Stores items that have magical properties or are enhanced in some way.';
