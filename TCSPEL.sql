3c1e5c1bcb206eaf972fa93f6d97cc11f074550a
Ian Brothers <33327410+Aarith@users.noreply.github.com>
2017-11-25 16:51:30 -0600

    Updated TCSPEL

--- TCSPEL.sql
+++ TCSPEL.sql
@@ -3,9 +3,9 @@
 CREATE TABLE `worldwide_market`.`spell` (
   `SpellID` INT NOT NULL,
   `Name` VARCHAR(45) NULL,
-  `Description` VARCHAR(45) NULL,
-  `School` VARCHAR(45) NULL,
-  `Effect` VARCHAR(45) NULL,
+  `Description` VARCHAR(200) NULL,
+  `School` VARCHAR(15) NULL,
+  `Effect` VARCHAR(15) NULL,
   `Components` VARCHAR(45) NULL,
   `Range` INT NULL,
   `Level` INT NULL,
