1234a15d5514c8b960c1ea88f499fcf20f314ac8
John M <johnswork15@gmail.com>
2017-11-25 16:06:24 -0600

    Comments
    Added a comment

--- --Database Creation.sql
+++ --Database Creation.sql
@@ -17,6 +17,7 @@ CREATE TABLE `worldwide_market`.`valstore` (
   `StoreType` VARCHAR(45) NOT NULL,
   `TypeDescription` VARCHAR(45) NULL,
   PRIMARY KEY (`StoreType`));
+COMMENT = 'Designed to hold the store types codes and a description of each code.'
 
 CREATE TABLE `worldwide_market`.`item` (
   `ItemID` INT NOT NULL AUTO_INCREMENT,
