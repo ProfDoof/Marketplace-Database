ce00974041ca741e39a68e9a1545dc62a55da139
John M <johnswork15@gmail.com>
2017-11-27 22:12:04 -0600

    Update
    Got rid of the percent signs in the %MarketDiff and %HaggleDiff due to issues it causes with the select statement

--- Database_Builder.sql
+++ Database_Builder.sql
@@ -11,8 +11,8 @@ CREATE TABLE `worldwide_market`.`stor` (
   `StoreName` VARCHAR(45) NOT NULL,
   `StoreType` VARCHAR(45) NOT NULL,
   `Location` VARCHAR(45) NOT NULL,
-  `%MarketDiff` INT NOT NULL,
-  `%HaggleDiff` INT NOT NULL,
+  `MarketDiff` INT NOT NULL,
+  `HaggleDiff` INT NOT NULL,
   PRIMARY KEY (`StoreID`))
 COMMENT = ' Designed to hold the store names and types among the worldwide marketplace ';
 
