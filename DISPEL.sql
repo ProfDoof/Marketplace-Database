484b04486497d36d870b4cba999f497f7c449999
Ian Brothers <ianb@taylortel.net>
2017-12-02 17:51:01 -0600

    SQL Creation
    Created the data insertion files for the SPEL table and the SPEL validation table

---
+++ DISPEL.sql
@@ -0,0 +1,70 @@
+-- Data Insertion Store Table
+
+INSERT INTO SPEL VALUES(
+	null, 'SpellName', 'SpellDesc', 'School', 'Effect', 'Components', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, 'Acid Splash', 'You may hurl an orb of acid at one target creature or two standing within 5 feet of each other. The orb deals 1d6 acid damage and increases by 1d6 at 5th, 11th, and 17th level.', 'CO', '1d6 Acid Damage', 'VS', 60, 0);
+
+INSERT INTO SPEL VALUES(
+	null, 'Blade Ward', 'You trace a sigil of warding in the air over yourself. Until the end of your next turn you have resistance to slashing, bludgeoning, and piercing attacks dealt by weapons.', 'AB', 'Grants resistance against physical attacks.', 'VS', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, 'Chill Touch', 'Create a ghostly, skeletal hand and make a ranged spell attack against a target creature. On a hit the creature takes 1d8 necrotic damage and cannot regain HP until the start of your next turn. If the target is undead, they also have disadvantage when attacking you until the end of your next turn. The damage inflicted increased by 1d8 at 5th, 11th, and 17th level.', 'NE', 'Deal 1d8 necrotic damage and prevent healing on target.', 'VS', 120, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
+
+INSERT INTO SPEL VALUES(
+	null, '', '', '', '', '', 0, 0);
---
+++ DISPVT.sql
@@ -0,0 +1,28 @@
+-- Data Insertion Spell Table
+
+INSERT INTO SPVT VALUES(
+	'AB', 'Abjuration');
+
+INSERT INTO SPVT VALUES(
+	'CO', 'Conjuration');
+
+INSERT INTO SPVT VALUES(
+	'DI', 'Divination');
+
+INSERT INTO SPVT VALUES(
+	'EN', 'Enchantment');
+
+INSERT INTO SPVT VALUES(
+	'EV', 'Evocation');
+
+INSERT INTO SPVT VALUES(
+	'IL', 'Illusion');
+
+INSERT INTO SPVT VALUES(
+	'NE', 'Necromancy');
+
+INSERT INTO SPVT VALUES(
+	'TR', 'Transmutation');
+
+INSERT INTO SPVT VALUES(
+	'UN', 'Universal');
