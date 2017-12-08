5d639b139e7e4907c279e428bd42006a3e49084f
Ian Brothers <ianb@taylortel.net>
2017-12-08 13:48:48 -0600

    Added more spells
    Added additional spells and descriptions to the data insertion form.

--- DISPEL.sql
+++ DISPEL.sql
@@ -13,16 +13,16 @@ INSERT INTO SPEL VALUES(
 	null, 'Chill Touch', 'Create a ghostly, skeletal hand and make a ranged spell attack against a target creature. On a hit the creature takes 1d8 necrotic damage and cannot regain HP until the start of your next turn. If the target is undead, they also have disadvantage when attacking you until the end of your next turn. The damage inflicted increased by 1d8 at 5th, 11th, and 17th level.', 'NE', 'Deal 1d8 necrotic damage and prevent healing on target.', 'VS', 120, 0);
 
 INSERT INTO SPEL VALUES(
-	null, '', '', '', '', '', 0, 0);
+	null, 'Create Bonfire', 'Creates a small bonfire at a location within range. Creatures standing where the bonfire is made must perform a DEX saving throw or take 1d8 fire damage. This damage is raised by 1d8 at 5th, 11th, and 17th level.', 'Conjuration', 'Create a bonfire at a target location within range.', 'VS', 60, 0);
 
 INSERT INTO SPEL VALUES(
-	null, '', '', '', '', '', 0, 0);
+	null, 'Dancing Lights', 'Create four small floating lights that can be combined into one vaguely humanoid figure. Each light shines for a 10 foot radius. The lights can be moved up to 60 feet at will, but must remain within 20 feet of each other and will disappear if they exit the spell\'s range.', 'Evocation', 'Create four small floating lights that shed dim light in a 10 foot radius.', 'VSM', 120, 0);
 
 INSERT INTO SPEL VALUES(
-	null, '', '', '', '', '', 0, 0);
+	null, 'Eldritch Blast', 'You create a crackling beam of energy that flies toward a target. On a hit, they take 1d10 force damage. You generate an additional beam at 5th, 11th, and 17th level. The extra beams can be directed at the same target or up to 3 more targets. Make a separate attack roll for each beam.', 'Evocation', 'Make a ranged spell attack against a target. On a hit, the target takes 1d10 force damage.', 'VS', 120, 0);
 
 INSERT INTO SPEL VALUES(
-	null, '', '', '', '', '', 0, 0);
+	null, 'Firebolt', 'Hurl a flaming mote of energy at a target. On a hit they take 1d10 fire damage. Also ignited unattended flammable objects. This spell gains an additional 1d10 damage at 5th, 11th, and 17th level.', 'Evocation', 'Make a ranged spell attack. On a hit your target takes 1d10 fire damage. Ignites unattended flammable objects.', 'VS', 120, 0);
 
 INSERT INTO SPEL VALUES(
 	null, '', '', '', '', '', 0, 0);
